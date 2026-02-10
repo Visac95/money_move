c19db246450d01082f7b22e3f32ae0f2601fdf4a
Visac95 <0979605259samu@gmail.com>
2026-02-10 17:57:58 -0500

    Refactor HomeScreen to StatefulWidget; implement tutorial screen with navigation and shared preferences

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:money_move/l10n/app_localizations.dart';
 import 'package:money_move/providers/transaction_provider.dart';
 import 'package:money_move/screens/loading_screen.dart';
+import 'package:money_move/screens/tutorial_app_screen.dart';
 import 'package:money_move/utils/mode_color_app_bar.dart';
 import 'package:money_move/widgets/add_button.dart';
 import 'package:money_move/widgets/balance_card.dart';
@@ -11,25 +12,53 @@ import 'package:money_move/widgets/settings_button.dart';
 import 'package:money_move/widgets/ultimas_deudas.dart';
 import 'package:money_move/widgets/ultimas_transacciones.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  SharedPreferences? prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    cargarPreferencias();
+  }
+
+  Future<void> cargarPreferencias() async {
+    prefs = await SharedPreferences.getInstance();
+    
+    if (mounted) {
+      final bool skipTutorial = prefs?.getBool("skipAppTutorial") ?? false;
+      
+      if (!skipTutorial) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => TutorialAppScreen()),
+        );
+      }
+      setState(() {}); 
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
     final tProvider = Provider.of<TransactionProvider>(context);
 
-    if (tProvider.isLoading) {
+    if (tProvider.isLoading || prefs == null) {
       return const LoadingScreen();
     }
 
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: modeColorAppbar(context, 0.4),
         elevation: 0,
-
         title: Text(
           AppLocalizations.of(context)!.appTitle,
           style: TextStyle(
@@ -40,10 +69,9 @@ class HomeScreen extends StatelessWidget {
         actions: [settingsButton(context)],
         leading: LeadingDrawer(),
       ),
-
       drawer: drawerUser(context),
       body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
+        physics: const BouncingScrollPhysics(),
         child: Column(
           children: [
             BalanceCard(
@@ -52,8 +80,8 @@ class HomeScreen extends StatelessWidget {
               incomeAmount: tProvider.totalIngresos,
               withFilterButton: false,
             ),
-            UltimasTransacciones(),
-            UltimasDeudas(),
+            const UltimasTransacciones(),
+            const UltimasDeudas(),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: CategoryPieChart(transactions: tProvider.transactions),
---
+++ lib/screens/tutorial_app_screen.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:money_move/config/app_colors.dart';
+import 'package:money_move/screens/home_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class TutorialAppScreen extends StatefulWidget {
+  const TutorialAppScreen({super.key});
+
+  @override
+  State<TutorialAppScreen> createState() => _TutorialAppScreenState();
+}
+
+class _TutorialAppScreenState extends State<TutorialAppScreen> {
+  final PageController _controller = PageController();
+  int _currentPage = 0;
+  final int _totalPages = 3;
+  void onChanged(double d) {}
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          // LAYER 1: The Sliding Content (Bottom)
+          PageView(
+            controller: _controller,
+            onPageChanged: (index) {
+              setState(() {
+                _currentPage = index;
+              });
+            },
+            children: [
+              // ... your pages here
+              Container(
+                color: colorScheme.surface,
+                child: Center(child: _page1()),
+              ),
+              Container(
+                color: colorScheme.primary,
+                child: Center(child: _page2()),
+              ),
+              Container(
+                color: AppColors.income,
+                child: Center(child: _page3()),
+              ),
+              Container(
+                color: colorScheme.inversePrimary,
+                child: Center(child: _page3()),
+              ),
+            ],
+          ),
+
+          // LAYER 2: The Skip Button (Top Right)
+          Positioned(
+            top: 0,
+            right: 0,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(
+                  16.0,
+                ), // Add some spacing from the edge
+                child: TextButton(
+                  onPressed: () {
+                    _finishTutorial(); // Call your save & exit function
+                  },
+                  style: TextButton.styleFrom(
+                    // Optional: Add a subtle background so it's visible on any color
+                    backgroundColor: Colors.black.withValues(alpha: 0.1),
+                    foregroundColor: Colors.white, // Text color
+                    shape: StadiumBorder(),
+                  ),
+                  child: const Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text("Skip"),
+                      SizedBox(width: 5),
+                      Icon(Icons.close, size: 18), // Nice "X" icon or arrow
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+
+          if (_currentPage > 0)
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: _buildCircleButton(
+                  icon: Icons.arrow_back_ios_new,
+                  onTap: () {
+                    _controller.previousPage(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                    );
+                  },
+                ),
+              ),
+            ),
+
+          // ---------------------------------------------
+          // LAYER 4: The RIGHT Arrow (Next / Finish)
+          // ---------------------------------------------
+          
+          Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: _buildCircleButton(
+                // Change icon if on last page
+                icon: _currentPage == _totalPages - 1
+                    ? Icons.check
+                    : Icons.arrow_forward_ios,
+                onTap: () {
+                  if (_currentPage == _totalPages - 1) {
+                    _finishTutorial(); // Finish if last page
+                  } else {
+                    _controller.nextPage(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeInOut,
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // 1. The Design of the Round Buttons
+  Widget _buildCircleButton({
+    required IconData icon,
+    required VoidCallback onTap,
+  }) {
+    return Material(
+      color: Colors.transparent, // Required for InkWell ripple
+      child: InkWell(
+        onTap: onTap,
+        customBorder: const CircleBorder(),
+        child: Container(
+          width: 50,
+          height: 50,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            // Semi-transparent background for contrast
+            color: Theme.of(context).colorScheme.surface.withOpacity(0.8),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.1),
+                blurRadius: 10,
+                offset: const Offset(0, 4),
+              ),
+            ],
+          ),
+          child: Icon(icon, color: Theme.of(context).colorScheme.primary),
+        ),
+      ),
+    );
+  }
+
+  void _finishTutorial() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('skipAppTutorial', true);
+    if (mounted) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    }
+  }
+}
+
+Widget _page1() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+
+    children: [Text("Bienvenido 1")],
+  );
+}
+
+Widget _page2() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [Text("Bienvenido 2")],
+  );
+}
+
+Widget _page3() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [Text("Bienvenido 3")],
+  );
+}
